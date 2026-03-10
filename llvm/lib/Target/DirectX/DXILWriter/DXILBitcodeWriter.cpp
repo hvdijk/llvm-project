bd0d81b141552025351417b6c6d75cccd1a67ed7
Harald van Dijk <hdijk@accesssoftek.com>
2026-03-10 17:46:29 +0000

    [DirectX] Do not emit !dbg on function definitions
    LLVM 3.7 did not do this. Instead, the metadata contains a reference to the function.

--- llvm/lib/Target/DirectX/DXILWriter/DXILBitcodeWriter.cpp
+++ llvm/lib/Target/DirectX/DXILWriter/DXILBitcodeWriter.cpp
@@ -1865,9 +1865,12 @@ void DXILBitcodeWriter::writeFunctionMetadataAttachment(const Function &F) {
   F.getAllMetadata(MDs);
   if (!MDs.empty()) {
     for (const auto &I : MDs) {
+      if (I.first == LLVMContext::MD_dbg) continue;
       Record.push_back(I.first);
       Record.push_back(VE.getMetadataID(I.second));
     }
+  }
+  if (!Record.empty()) {
     Stream.EmitRecord(bitc::METADATA_ATTACHMENT, Record, 0);
     Record.clear();
   }
